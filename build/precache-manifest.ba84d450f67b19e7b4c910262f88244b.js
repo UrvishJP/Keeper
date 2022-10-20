20f7b88ae7ba6b6891a1181eda5e9d668e042839
Urvish Prajapati <urvishmeghani111@gmail.com>
2022-10-20 21:30:50 +0530

    first commit

---
+++ build/precache-manifest.ba84d450f67b19e7b4c910262f88244b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdf5e4236be8d91ddb34a9dafd28246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078021d2b5d24e67004d",
+    "url": "/static/js/2.d6c23bdf.chunk.js"
+  },
+  {
+    "revision": "65a802ae3cb390b98f4f",
+    "url": "/static/js/main.23850695.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
